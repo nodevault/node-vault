a168210eae75ba705beb4534de0d2bff1eec5e0b
Krispin Schulz <krispinone@googlemail.com>
2016-07-06 23:51:33 +0200

    add example for passing request options

--- example/auth.js
+++ example/auth.js
@@ -4,6 +4,12 @@ process.env.DEBUG = 'node-vault'; // switch on debug mode
 
 const vault = require('./../src/index')();
 
-vault.auths()
+const options = {
+  requestOptions: {
+    followAllRedirects: true,
+  },
+};
+
+vault.auths(options)
 .then(console.log)
 .catch(console.error);
