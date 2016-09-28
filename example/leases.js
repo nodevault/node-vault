277eeb7e815de6a3ff541be8e6f378f1aa98e963
Krispin Schulz <krispinone@googlemail.com>
2016-09-28 10:33:31 +0200

    TODO fix example for leases because generic backend does not generate a lease_id anymore

--- example/leases.js
+++ example/leases.js
@@ -3,6 +3,9 @@
 process.env.DEBUG = 'node-vault'; // switch on debug mode
 const vault = require('./../src/index')();
 
+// TODO fix example for leases because generic backend does not generate a lease_id anymore
+// See: https://github.com/hashicorp/vault/issues/877
+
 vault.write('secret/hello', { value: 'world', lease: '10h' })
 .then(() => vault.read('secret/hello'))
 .then((result) => vault.revoke({ lease_id: result.lease_id }))
