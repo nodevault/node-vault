b21b748a28b4e281011219922825b7f15d3bc8d5
Sidharth Vinod <sidharthv96@gmail.com>
2020-07-23 18:05:15 +0530

    Remove await/async

--- scripts/gen-types.js
+++ scripts/gen-types.js
@@ -4,39 +4,40 @@ const { format } = require('json-schema-to-typescript/dist/src/formatter');
 const commands = require('../src/commands');
 const fs = require('fs');
 
-async function run() {
-  let types = '';
-  // TODO : Generate complete index.d.ts automatically
-  let commandDefs = `
-interface CopyPasteContentIntoIndex { \n
-`;
-  const commandNames = Object.keys(commands);
-  commandNames.sort();
-  for (const commandName of commandNames) {
-    const command = commands[commandName];
-    let reqOpt = 'Option';
-    let resOpt = 'unknown';
-    if (command.schema) {
-      for (const schemaType of Object.keys(command.schema)) {
-        const typeName = `${commandName}_${schemaType}`;
-        if (schemaType === 'req' || schemaType === 'query') {
-          reqOpt = toSafeString(`${commandName}_${schemaType}`);
-        } else if (schemaType === 'res') {
-          resOpt = toSafeString(`${commandName}_${schemaType}`);
-        }
-        types += await compile(command.schema[schemaType], typeName, {
-          bannerComment: '',
-        });
+const compilePromises = [];
+// TODO : Generate complete index.d.ts automatically
+const commandDefs = ['interface CopyPasteContentIntoIndex {'];
+const commandNames = Object.keys(commands);
+commandNames.sort();
+for (const commandName of commandNames) {
+  const command = commands[commandName];
+  let reqOpt = 'Option';
+  let resOpt = 'unknown';
+  if (command.schema) {
+    for (const schemaType of Object.keys(command.schema)) {
+      const typeName = `${commandName}_${schemaType}`;
+      if (schemaType === 'req' || schemaType === 'query') {
+        reqOpt = toSafeString(`${commandName}_${schemaType}`);
+      } else if (schemaType === 'res') {
+        resOpt = toSafeString(`${commandName}_${schemaType}`);
       }
+      compilePromises.push(
+        compile(command.schema[schemaType], typeName, {
+          bannerComment: '',
+        })
+      );
     }
-    commandDefs += `${commandName}(options?: ${reqOpt}): Promise<${resOpt}>;\n`;
   }
-
-  commandDefs += `}`;
-
-  types = types.replace(/export /g, '');
-  fs.writeFileSync(`types/types.ts`, format(types, DEFAULT_OPTIONS));
-  fs.writeFileSync(`types/commands.ts`, format(commandDefs, DEFAULT_OPTIONS));
+  commandDefs.push(`${commandName}(options?: ${reqOpt}): Promise<${resOpt}>;`);
 }
 
-run();
+commandDefs.push('}');
+fs.writeFileSync(
+  'types/commands.ts',
+  format(commandDefs.join('\n'), DEFAULT_OPTIONS)
+);
+
+Promise.all(compilePromises).then((types) => {
+  const typeString = types.join('').replace(/export /g, '');
+  fs.writeFileSync('types/types.ts', format(typeString, DEFAULT_OPTIONS));
+});
