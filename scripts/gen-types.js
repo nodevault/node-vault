2c7a18bad84168b076453bacb86cb8a6d9469c80
Sidharth Vinod <sidharthv96@gmail.com>
2020-07-23 15:21:40 +0530

    Cleanup

--- scripts/gen-types.js
+++ scripts/gen-types.js
@@ -7,36 +7,36 @@ const fs = require('fs');
 async function run() {
   let types = '';
   // TODO : Generate complete index.d.ts automatically
-  let funs = `
+  let commandDefs = `
 interface CopyPasteContentIntoIndex { \n
 `;
   const commandNames = Object.keys(commands);
   commandNames.sort();
-  for (k of commandNames) {
-    const c = commands[k];
+  for (const commandName of commandNames) {
+    const command = commands[commandName];
     let reqOpt = 'Option';
     let resOpt = 'unknown';
-    if (c.schema) {
-      for (schemaType of Object.keys(c.schema)) {
-        const typeName = `${k}_${schemaType}`;
+    if (command.schema) {
+      for (const schemaType of Object.keys(command.schema)) {
+        const typeName = `${commandName}_${schemaType}`;
         if (schemaType === 'req' || schemaType === 'query') {
-          reqOpt = toSafeString(`${k}_${schemaType}`);
+          reqOpt = toSafeString(`${commandName}_${schemaType}`);
         } else if (schemaType === 'res') {
-          resOpt = toSafeString(`${k}_${schemaType}`);
+          resOpt = toSafeString(`${commandName}_${schemaType}`);
         }
-        types += await compile(c.schema[schemaType], typeName, {
+        types += await compile(command.schema[schemaType], typeName, {
           bannerComment: '',
         });
       }
     }
-    funs += `${k}(options?: ${reqOpt}): Promise<${resOpt}>;\n`;
+    commandDefs += `${commandName}(options?: ${reqOpt}): Promise<${resOpt}>;\n`;
   }
 
-  funs += `}`;
+  commandDefs += `}`;
 
   types = types.replace(/export /g, '');
   fs.writeFileSync(`types/types.ts`, format(types, DEFAULT_OPTIONS));
-  fs.writeFileSync(`types/commands.ts`, format(funs, DEFAULT_OPTIONS));
+  fs.writeFileSync(`types/commands.ts`, format(commandDefs, DEFAULT_OPTIONS));
 }
 
 run();
