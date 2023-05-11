f8220b0129c2337a9ce984c5117819b0560c92e6
Aviad Hahami <ahahami@paloaltonetworks.com>
2023-05-11 16:41:11 +0300

    chore: mass lint

--- test/integration.js
+++ test/integration.js
@@ -12,33 +12,33 @@ const tv4 = require('tv4');
 const assert = require('assert');
 
 const validResult = (commandName, result) => {
-  const schema = commands[commandName].schema.res;
-  return tv4.validate(result, schema);
+    const schema = commands[commandName].schema.res;
+    return tv4.validate(result, schema);
 };
 
 describe('integration', () => {
-  describe('node-vault', () => {
-    it('should initialize a vault server', (done) => {
-      vault.init({ secret_shares: 1, secret_threshold: 1 })
-      .then((result) => {
-        vault.token = result.root_token;
-        assert(validResult('init', result));
-        return done();
-      })
-      .catch((err) => {
-        if (err.message === 'Vault is already initialized') {
-          return done();
-        }
-        return done(err);
-      });
+    describe('node-vault', () => {
+        it('should initialize a vault server', (done) => {
+            vault.init({ secret_shares: 1, secret_threshold: 1 })
+                .then((result) => {
+                    vault.token = result.root_token;
+                    assert(validResult('init', result));
+                    return done();
+                })
+                .catch((err) => {
+                    if (err.message === 'Vault is already initialized') {
+                        return done();
+                    }
+                    return done(err);
+                });
+        });
+        it('should show the current status of the vault server', (done) => {
+            vault.status()
+                .then((result) => {
+                    assert(validResult('status', result));
+                    return done();
+                })
+                .catch(done);
+        });
     });
-    it('should show the current status of the vault server', (done) => {
-      vault.status()
-      .then((result) => {
-        assert(validResult('status', result));
-        return done();
-      })
-      .catch(done);
-    });
-  });
 });
