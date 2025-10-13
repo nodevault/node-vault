ac4d75bf240a0fad1e93f69d6a9174cd625cf5c9
skeletonz28 <33682551+skeletonz28@users.noreply.github.com>
2025-10-13 10:37:35 +0200

    Update update.js
    Adding more options for update

--- example/update.js
+++ example/update.js
@@ -11,3 +11,7 @@ vault.update('secret/hello', { value: 'world', lease: '1s' })
 vault.update('secret/hello', { value: 'everyone', lease: '1s' })
     .then(() => vault.read('secret/hello'))
     .catch((err) => console.error(err.message));
+
+vault.update('secret/hello', { value: { 'foo': 'bar', 'world': 'everyone' } })
+    .then(() => vault.read('secret/hello'))
+    .catch((err) => console.error(err.message));
